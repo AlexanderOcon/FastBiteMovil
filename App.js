73f03147fb67504e674f1a796b62b97285de7da6
AlexanderOcon <brandon.hermida.ocon@gmail.com>
2026-09-07 10:48:32 -0600

    Archivos y Capetas

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Subidatest from './components/Subidatest';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Subidatest />
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -13,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fceeeeb3b',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ components/Subidatest.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Subidatest = () => {
+  return (
+    <View>
+      <Text>Subidatest</Text>
+    </View>
+  )
+}
+
+export default Subidatest
+
+const styles = StyleSheet.create({})
